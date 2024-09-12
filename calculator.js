2912150343ff270e3420516f2295c1fe44ec27d4
Raad Remon <70149212+Remon009534@users.noreply.github.com>
2024-09-12 22:37:38 +0600

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,21 @@
+const screen = document.querySelector('.screen');
+
+function appendToDisplay(input)
+{
+    screen.value += input;
+}
+function clearDisplay()
+{
+    screen.value = '';
+}
+function calculate()
+{
+    try
+    {
+        screen.value = eval(screen.value);
+    }
+    catch(error)
+    {
+        screen.value = "Error";
+    }
+}
